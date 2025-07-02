1552f6243df1a216c5e2ae373a99d632f4817fdb
Dima_101 <162629126+dmytroustymovych@users.noreply.github.com>
2025-07-03 00:07:00 +0300

    Add files via upload

---
+++ Basics/7 INSERT and SELECT Data/Models/Computer.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+        public class Computer
+        {
+            // When creating classes, id is a must 90% of the time(sourse: me)  
+            public int IdComputer { get; set; }
+
+            private int ComputerID { get; set; } = 1;
+
+            public string Motherboard { get; set; } = "";
+
+            public int CPUCores { get; set; }
+
+            public bool HasWifi { get; set; }
+
+            public bool HasLTE { get; set; }
+
+            // Could also use DateTime, but it is better if want to have precise time
+            public DateOnly ReleaseDate { get; set; }
+
+            public Decimal Price { get; set; }
+
+            public string VideoCard { get; set; } = "";
+
+            public Computer()
+            {
+                // Nice Autoincrementor(just like in most SQL languages)
+                IdComputer = ComputerID++;
+            }
+
+            // I mean, if u want to make a computer example, you can just create a method for imputing data
+            // Though never do it like I did. It's just an example; Also I didn't do null checkers
+            public static Computer CreateComputer()
+            {
+                string Motherboard;
+                int CPUCores;
+                bool HasWifi;
+                bool HasLTE;
+                DateOnly ReleaseDate;
+                Decimal Price;
+                string VideoCard;
+
+                Console.WriteLine("Creating a Computer...");
+
+                Console.WriteLine("Input Motherboard");
+                Motherboard = Console.ReadLine();
+
+                Console.WriteLine("Input CPUCores");
+                CPUCores = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Input if it has WiFi");
+                HasWifi = bool.Parse(Console.ReadLine());
+
+                Console.WriteLine("Input if it has LTE");
+                HasLTE = bool.Parse(Console.ReadLine());
+
+                Console.WriteLine("Input ReleaseDate");
+                ReleaseDate = DateOnly.Parse(Console.ReadLine());
+                DateTime RD_DateTime = ReleaseDate.ToDateTime(TimeOnly.MinValue);
+
+                // What I wasn't prepared for, this functin wanted "," as separator, not "." (like 12,14)
+                Console.WriteLine("Input Price");
+                Price = Decimal.Parse(Console.ReadLine());
+
+                Console.WriteLine("Input VideoCard");
+                VideoCard = Console.ReadLine();
+
+                // Saving data in console
+                var computer = new Computer()
+                    {
+                        Motherboard = Motherboard,
+                        CPUCores = CPUCores,
+                        HasWifi = HasWifi,
+                        HasLTE = HasLTE,
+                        ReleaseDate = ReleaseDate,
+                        Price = Price,
+                        VideoCard = VideoCard
+                };
+
+                // Saving data in SQL Server
+                string MSSqlconnectionString = "Data Source = localhost\\SQLEXPRESS; Initial Catalog = Practice; Integrated Security = True; Trust Server Certificate = True";
+                SqlConnection MSSqlconnection = new SqlConnection(MSSqlconnectionString);
+                MSSqlconnection.Open();
+
+                // Don't know why, but it dosen't work, says There are fewer columns in the INSERT statement than values specified in the VALUES clause
+                // Will try the other way
+                //string SqlCommand = "INSERT INTO Project.Computer(Motherboard, CPUcores, HasWiFi, HasLTE, Release_Date, Price, VideoCard) VALUES " +
+                //$"('{Motherboard}', {CPUCores}, {(HasWifi ? 1 : 0)}, {(HasLTE ? 1 : 0)}, '{RD_DateTime:yyyy-MM-dd}', {Price}, '{VideoCard}')";
+                string SqlCommand = "INSERT INTO Project.Computer(Motherboard, CPUcores, HasWiFi, HasLTE, Release_Date, Price, VideoCard) VALUES " +
+                "(@Motherboard, @CPUCores, @HasWiFi, @HasLTE, @ReleaseDate, @Price, @VideoCard)";
+                SqlCommand command = new SqlCommand(SqlCommand, MSSqlconnection);
+
+                command.Parameters.AddWithValue("@Motherboard", Motherboard);
+                command.Parameters.AddWithValue("@CPUCores", CPUCores);
+                command.Parameters.AddWithValue("@HasWifi", HasWifi);
+                command.Parameters.AddWithValue("@HasLTE", HasLTE);
+                command.Parameters.AddWithValue("@ReleaseDate", ReleaseDate);
+                command.Parameters.AddWithValue("@Price", Price);
+                command.Parameters.AddWithValue("@VideoCard", VideoCard);
+
+                command.ExecuteNonQuery();
+                Console.WriteLine("Computer created successfully!");
+
+            return computer;
+            }
+        }
+
+    
+
+}
---
+++ Basics/7 INSERT and SELECT Data/Program.cs
@@ -0,0 +1,90 @@
+﻿using ConsoleApp1.Models;
+using Google.Protobuf.WellKnownTypes;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Tls;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.Collections;
+//using Dapper;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            //// Way #1 (using SQL Server directly)
+            //// Connection string
+            //string MSSqlconnectionString = "Data Source = localhost\\SQLEXPRESS; Initial Catalog = Practice; Integrated Security = True; Trust Server Certificate = True";
+            //// Made a connection
+            //SqlConnection MSSqlconnection = new SqlConnection(MSSqlconnectionString);
+            //// Opening a connection with a database
+            //MSSqlconnection.Open();
+
+            ////string sqlCommand = "INSERT INTO Project.Computer(CPUcores) VALUES(2);";
+            ////SqlCommand command = new SqlCommand(sqlCommand, MSSqlconnection);
+            ////var query = command.;
+
+            //string sqlCommand1 = "SELECT * FROM Project.Computer Where CPUcores = 3";
+            //SqlCommand nextcommand = new SqlCommand(sqlCommand1, MSSqlconnection);
+
+            //// Я також змінив функцію CreateComputer для створення нового об'єкта в БД
+            //var computer1 = Computer.CreateComputer();
+
+            ////SqlDataReader query1 = nextcommand.ExecuteReader();
+            //var reader = nextcommand.ExecuteReader();
+
+            //while(reader.Read())
+            //{
+            //    Console.Write($"IDComputer: {reader["id_computer"]}");
+            //    Console.Write($" Motherboard: {reader["Motherboard"]}");
+            //    Console.Write($" CPUCores: {reader["CPUCores"]}");
+            //    Console.Write($" HasWiFi: {reader["HasWiFi"]}");
+            //    Console.Write($" HasLTE: {reader["HasLTE"]}");
+            //    Console.Write($" ReleaseDate: {reader["Release_Date"]}");
+            //    Console.Write($" Price: {reader["Price"]}");
+            //    Console.Write($" VideoCard: {reader["VideoCard"]}\n");
+            //}
+
+
+            //while (reader.Read())
+            //{
+            //    Console.WriteLine($"CPU Cores: {reader["CPUcores"]}");
+            //}
+            ////Console.WriteLine($"Data Inserted: {reader}\n");
+
+            //// Way #2 (using MySQL directly)
+            //// Connection string
+            //string MySQLconnectionString = "Server=localhost;Database=Practice;User ID=root;Password=hgh$&^489hbjkpio*;";
+            //// Made a connection
+            //MySqlConnection MySqlconnection = new MySqlConnection(MySQLconnectionString);
+            //// Opening a connection with a database
+            //MySqlconnection.Open();
+            //// Made a Command
+            //MySqlCommand command1 = new MySqlCommand("SELECT NOW();", MySqlconnection);
+            //var result = command1.ExecuteScalar();
+            //Console.WriteLine($"Current MySQL server time: {result}");
+
+            // I don't want to use Dapper right now, so I will just use the native SQL Server and MySQL packages
+            // Maybe I will implement Dapper later
+
+            //var computer1 = Computer.CreateComputer();
+            //Console.WriteLine(computer1);
+            //// No, I don't want to make a second method just to output data, too lazy
+            //Console.WriteLine($"IdComputer: {computer1.IdComputer}");
+            //Console.WriteLine($"Motherboard: {computer1.Motherboard}");
+            //Console.WriteLine($"CPUCores: {computer1.CPUCores}");
+            //Console.WriteLine($"HasWifi: {computer1.HasWifi}");
+            //Console.WriteLine($"HasLTE: {computer1.HasLTE}");
+            //Console.WriteLine($"ReleaseDate: {computer1.ReleaseDate}");
+            //Console.WriteLine($"Price: {computer1.Price}");
+            //Console.WriteLine($"VideoCard: {computer1.VideoCard}");
+
+
+            string[] colors = { "green", "brown", "blue", "red" };
+            Console.WriteLine(colors.OrderBy(c => c.Length).First());
+
+        }
+    }
+}
